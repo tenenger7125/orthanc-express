6c1226fe96a02202a3e1938795e461abb9b2f83e
LEE DONG GYU <tenenger7125@gmail.com>
2025-06-28 20:49:22 +0900

    🎉 Express 초기 세팅

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+// app.ts
+
+import express from 'express';
+
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(app.get('port'), () => {
+  console.log(app.get('port'), '번에서 대기중');
+});
